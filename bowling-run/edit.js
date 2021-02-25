5eab492c370b516b47989e2ac1dd2cbd84b96218
Tatsuya Ishii <tishii2479@gmail.com>
2021-02-25 22:57:34 +0900

    ruler

--- bowling-run/edit.js
+++ bowling-run/edit.js
@@ -1,8 +1,8 @@
 let gimmickData;
-let selectedIndex;
 let selectedTile;
 let selectedGimmickIdx = -1;
 let isRemoving = false;
+let gimmicks;
 
 const H = 200;
 const W = 9;
@@ -11,6 +11,7 @@ const tilePalette = document.getElementById("tile_palette");
 const status = document.getElementById("status");
 const create = document.getElementById("create");
 const remove = document.getElementById("remove");
+const ruler = document.getElementById("ruler");
 const tiles = [
     "1",
     "2",
@@ -191,6 +192,7 @@ const gimmickConstraints = {
 
 setUpToolBar();
 setUpStage();
+setUpRuler();
 updateData("");
 
 function updateData(data) {
@@ -235,16 +237,41 @@ function reloadView() {
     }
 
     stage.innerHTML = result;
-    const gimmicks = document.getElementsByClassName("gimmick");
+    gimmicks = document.getElementsByClassName("gimmick");
+
+    // //マウスが要素内で押されたとき、又はタッチされたとき発火
+    // for(let i = 0; i < gimmicks.length; i++) {
+    //     gimmicks[i].addEventListener("mousedown", mdown, false);
+    //     gimmicks[i].addEventListener("touchstart", mdown, false);
+    // }
 
     // それぞれのギミックにクリックイベントを追加する
     for (let i = 0; i < gimmicks.length; i++) {
         gimmicks[i].addEventListener("click", function () {
+            // selectedTile = gimmicks[i].id;
             if (isRemoving == false) { return; }
             gimmickData.splice(i, 1);
             reloadView();
         });
     }
+
+    status.innerHTML = `<p>
+        <b>選択中: ${selectedTile}</b>
+        </p>
+    `;
+}
+
+function setUpRuler() {
+    let result = "";
+    for (let i = 0; i <= 20; i++) {
+        result += `
+            <div class="ruler-block">
+                <p>${i * 100}</p>
+            </div>
+        `;
+    }
+
+    ruler.innerHTML = result;
 }
 
 function setUpStage() {
@@ -291,7 +318,6 @@ function setUpToolBar() {
     for (let i = 0; i < toolbarTiles.length; i++) {
         toolbarTiles[i].addEventListener("click", function () {
             selectedTile = tiles[i];
-            selectedIndex = i;
             isRemoving = false;
             status.innerHTML = `<p>
                 <b>選択中: ${tiles[i]}</b>
@@ -334,3 +360,71 @@ const onBeforeUnloadHandler = function (e) {
 };
 
 window.addEventListener('beforeunload', onBeforeUnloadHandler, false);
+
+// ギミックのドラッグ
+// https://q-az.net/elements-drag-and-drop/
+
+// //要素内のクリックされた位置を取得するグローバル（のような）変数
+// let x;
+// let y;
+
+// //マウスが押された際の関数
+// function mdown(e) {
+//     //クラス名に .drag を追加
+//     this.classList.add("drag");
+
+//     //タッチデイベントとマウスのイベントの差異を吸収
+//     let event;
+//     if (e.type === "mousedown") {
+//         event = e;
+//     } else {
+//         event = e.changedTouches[0];
+//     }
+
+//     //要素内の相対座標を取得
+//     x = event.pageX - this.offsetLeft;
+//     y = event.pageY - this.offsetTop;
+
+//     //ムーブイベントにコールバック
+//     document.body.addEventListener("mousemove", mmove, false);
+//     document.body.addEventListener("touchmove", mmove, false);
+// }
+
+// //マウスカーソルが動いたときに発火
+// function mmove(e) {
+//     //ドラッグしている要素を取得
+//     let drag = document.getElementsByClassName("drag")[0];
+
+//     let event;
+//     //同様にマウスとタッチの差異を吸収
+//     if(e.type === "mousemove") {
+//         event = e;
+//     } else {
+//         event = e.changedTouches[0];
+//     }
+
+//     //フリックしたときに画面を動かさないようにデフォルト動作を抑制
+//     e.preventDefault();
+
+//     //マウスが動いた場所に要素を動かす
+//     drag.style.top = event.pageY - y + "px";
+//     drag.style.left = event.pageX - x + "px";
+
+//     //マウスボタンが離されたとき、またはカーソルが外れたとき発火
+//     drag.addEventListener("mouseup", mup, false);
+//     drag.addEventListener("touchend", mup, false);
+// }
+
+// //マウスボタンが上がったら発火
+// function mup(e) {
+//     let drag = document.getElementsByClassName("drag")[0];
+
+//     //ムーブベントハンドラの消去
+//     document.body.removeEventListener("mousemove", mmove, false);
+//     document.body.removeEventListener("touchmove", mmove, false);
+//     if (drag) {
+//         drag.removeEventListener("mouseup", mup, false);
+//         drag.removeEventListener("touchend", mup, false);
+//         drag.classList.remove("drag");
+//     }
+// }
